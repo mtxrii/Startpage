00780c2973c7ee93d2e9b2a42ae3783116b64717
Enrique <edavalos@ucsc.edu>
2020-08-11 20:19:22 -0700

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,73 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>
+      My name is <input v-model="name">
+    </p>
+    <p>
+      <button @click="persist">Save</button>
+    </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {},
+
+  el: '#app',
+
+  data: function() {
+    return {
+      name: '',
+      github: '',
+      todo: [],
+      newTodo: null
+    }
+  },
+
+  mounted() {
+    if (localStorage.name) {
+      this.name = localStorage.name;
+    }
+    if (localStorage.github) {
+      this.github = localStorage.github;
+    }
+    if (localStorage.getItem('todo')) {
+      try {
+        this.todo = JSON.parse(localStorage.getItem('todo'));
+      } catch(e) {
+        localStorage.removeItem('todo');
+      }
+    }
+  },
+
+  methods: {
+    persist() {
+      localStorage.name = this.name;
+      localStorage.github = this.github;
+    },
+
+    addTodo() {
+      if (!this.newTodo) return;
+
+      this.todo.push(this.newTodo);
+      this.newTodo = '';
+      this.saveTodo();
+    },
+
+    removeTodo(x) {
+      this.todo.splice(x, 1);
+      this.saveTodo();
+    },
+
+    saveTodo() {
+      const parsed = JSON.stringify(this.todo);
+      localStorage.setItem('todo', parsed);
+    }
+  },
+
 }
 </script>
 

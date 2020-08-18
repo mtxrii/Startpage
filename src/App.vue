6de0dbc96cc36d0600cbcc27bdf75ebac97a0a4b
Enrique <edavalos@ucsc.edu>
2020-08-17 18:27:59 -0700

    added card structure

--- src/App.vue
+++ src/App.vue
@@ -8,93 +8,105 @@
     <b-button id="editIcon1" type="is-dark" icon-right="account-edit" @click="openNameSettings" />
     <b-button id="editIcon2" type="is-dark" icon-right="github" @click="openGitSettings" />
 
-    <div class="columns">
-      <div class="column"> <!-- (1) github & weather -->
-        <div>
-          <p class="title is-5">Today's Github commit</p>
-          <div v-if="commit" class="commit full"></div>
-          <div v-else class="commit"></div>
-        </div>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
 
-        <br>
-        <br>
-
-        <div>
-          <p class="title is-5">Weather</p>
-          <p class="subtitle is-6"><b-icon icon="map-marker" size="is-small"></b-icon> {{city}}</p>
-
-          <figure style="margin-top: -30px; margin-bottom: -5px">
-            <img :src="weather.icon">
-          </figure>
-
-          <p>Max: <strong>{{weather.max}}</strong>  Min: <strong>{{weather.min}}</strong></p>
-          <p>Feels like: <strong>{{weather.feels_like}}</strong></p>
-          <p>Humidity: <strong>{{weather.humidity}}%</strong></p>
-        </div>
-      </div>
-
-      <div class="column is-two-fifths"> <!-- (2) bookmark tiles -->
-        <div class="columns is-mobile is-multiline is-centered">
-
-          <div class="column is-narrow">
-            <a @click="openNewLinkMenu()">
-              <div class="card">
-                <div class="card-content">
-                  <div class="media">
-                    <div class="media-left">
-                      <figure class="image is-32x32" style="padding-top: 3px">
-                        <img src="https://www.pikpng.com/pngl/m/4-49677_add-button-with-plus-symbol-in-a-black.png" alt="plus icon">
-                      </figure>
-                    </div>
-                    <div class="media-content">
-                      <p class="title is-6" style="padding-top: 9px; padding-bottom: 9px">Add Link</p>
-                    </div>
-                  </div>
-                </div>
+          <div class="column"> <!-- (1) github & weather -->
+            <div class="card">
+              <div class="card-content">
+                <p class="title is-5">Today's Github commit</p>
+                <div v-if="commit" class="commit full"></div>
+                <div v-else class="commit"></div>
               </div>
-            </a>
+            </div>
+
+            <br>
+            <br>
+
+            <div class="card">
+              <div class="card-content">
+                <p class="title is-5">Weather</p>
+                <p class="subtitle is-6"><b-icon icon="map-marker" size="is-small"></b-icon> {{city}}</p>
+
+                <figure style="margin-top: -30px; margin-bottom: -5px">
+                  <img :src="weather.icon">
+                </figure>
+
+                <p>Max: <strong>{{weather.max}}</strong>  Min: <strong>{{weather.min}}</strong></p>
+                <p>Feels like: <strong>{{weather.feels_like}}</strong></p>
+                <p>Humidity: <strong>{{weather.humidity}}%</strong></p>
+              </div>
+            </div>
           </div>
 
-          <div class="column is-narrow">
-            <a href="https://www.reddit.com/r/UCSC/">
-              <div class="card">
-                <div class="card-content">
-                  <div class="media">
-                    <div class="media-left">
-                      <figure class="image is-32x32" style="padding-top: 3px">
-                        <img src="https://www.google.com/s2/favicons?domain=reddit.com" alt="Favicon">
-                      </figure>
-                    </div>
-                    <div class="media-content">
-                      <p class="title is-6">Reddit</p>
-                      <p class="subtitle is-7">reddit.com</p>
+          <div class="column is-two-fifths"> <!-- (2) bookmark tiles -->
+            <div class="columns is-mobile is-multiline is-centered">
+
+              <div class="column is-narrow">
+                <a @click="openNewLinkMenu()">
+                  <div class="card">
+                    <div class="card-content">
+                      <div class="media">
+                        <div class="media-left">
+                          <figure class="image is-32x32" style="padding-top: 3px">
+                            <img src="https://www.pikpng.com/pngl/m/4-49677_add-button-with-plus-symbol-in-a-black.png" alt="plus icon">
+                          </figure>
+                        </div>
+                        <div class="media-content">
+                          <p class="title is-6" style="padding-top: 9px; padding-bottom: 9px">Add Link</p>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
+                </a>
               </div>
-            </a>
+
+              <div class="column is-narrow">
+                <a href="https://www.reddit.com/r/UCSC/">
+                  <div class="card">
+                    <div class="card-content">
+                      <div class="media">
+                        <div class="media-left">
+                          <figure class="image is-32x32" style="padding-top: 3px">
+                            <img src="https://www.google.com/s2/favicons?domain=reddit.com" alt="Favicon">
+                          </figure>
+                        </div>
+                        <div class="media-content">
+                          <p class="title is-6">Reddit</p>
+                          <p class="subtitle is-7">reddit.com</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+              
+            </div>
           </div>
-          
+
+          <div class="column"> <!-- (3) todo list -->
+            <div class="card">
+              <div class="card-content">
+                <div v-for="(i, n) in todo" v-bind:item="i" v-bind:index="n" v-bind:key="i.id" style="text-align: left;">
+                  <b-button rounded style="margin-top: 2px; margin-right: 2px" size="is-small" icon-right="check-outline" @click="removeTodo(n)"></b-button>
+                  <b-tag size="is-medium">{{i}}</b-tag>
+                  <div style="padding-top: 5px"></div>
+                </div>
+
+                <p>
+                  <b-field>
+                    <b-input v-model="newTodo"></b-input>
+                    <b-button @click="addTodo">Add Todo</b-button>
+                  </b-field>
+                </p> 
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
-
-      <div class="column"> <!-- (3) todo list -->
-        <div v-for="(i, n) in todo" v-bind:item="i" v-bind:index="n" v-bind:key="i.id" style="text-align: left;">
-          <b-button rounded style="margin-top: 2px; margin-right: 2px" size="is-small" icon-right="check-outline" @click="removeTodo(n)"></b-button>
-          <b-tag size="is-medium">{{i}}</b-tag>
-          <div style="padding-top: 5px"></div>
-        </div>
-
-        <p>
-          <b-field>
-            <b-input v-model="newTodo"></b-input>
-            <b-button @click="addTodo">Add Todo</b-button>
-          </b-field>
-        </p>
-      </div>
-    </div>
-
-
+    </section>
   </div>
 </template>
 
@@ -341,5 +353,9 @@ export default {
 
 .commit.full {
   background-color: rgb(155, 233, 168);
+}
+
+.card.transparent {
+  background-color: rgba(255, 255, 255, 50%);
 }
 </style>
